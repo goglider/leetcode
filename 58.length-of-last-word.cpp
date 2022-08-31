b4941e6b436712da9ab3a6d47f19edf9483e60e4
Parth Khurana <parthkhurana1@gmail.com>
2022-08-31 14:40:26 +0530

    solution

---
+++ 58.length-of-last-word.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=58 lang=cpp
+ *
+ * [58] Length of Last Word
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0, i;
+        for(i=s.size()-1;s[i]==' ';i--);
+        for(int j=0;j<=i;j++)
+        {
+            count++;
+            if(s[j]==' ')
+            {
+                count=0;
+            }
+        }
+        return count;
+    }
+};
+// @lc code=end
+
