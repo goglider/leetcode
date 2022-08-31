7d835d3214096d90c1ae0386fa85cc48598a1f34
Parth Khurana <parthkhurana1@gmail.com>
2022-08-31 18:28:05 +0530

    solution

---
+++ 5.longest-palindromic-substring.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=5 lang=cpp
+ *
+ * [5] Longest Palindromic Substring
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int LengthfromMid(string str, int left, int right)
+    {
+        while(left>=0&&right<str.size()&&str[left]==str[right])
+        {
+            left--;
+            right++;
+        }
+        return right-left-1;
+    }
+    string longestPalindrome(string s) {
+        if(s==""||s.size()<1)
+            return "";
+        if(s.size()==1)
+            return s;
+        int len=0;
+        int start=0;
+        int end=0;
+        for(int i=0;i<s.size();i++)
+        {
+            int len1 = LengthfromMid(s, i, i);
+            int len2 = LengthfromMid(s, i, i+1);
+            len = std::max(len1, len2);
+            if (len>end-start)
+            {
+                start=i-(len-1)/2;
+                end=i+(len/2);
+            }   
+        }
+        return s.substr(start, end-start+1);
+    }
+};
+// @lc code=end
+
--- 58.length-of-last-word.cpp
+++ 58.length-of-last-word.cpp
@@ -22,4 +22,3 @@ public:
     }
 };
 // @lc code=end
-
