320c21047cc6f9ca706a55fedecb43f9b2b8a1a6
Parth Khurana <parthkhurana1@gmail.com>
2022-10-05 19:08:39 +0530

    solution

---
+++ 121.best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=121 lang=cpp
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy=prices.at(0), sell=prices.at(0);
+        int res=0;
+        for(int i=0;i<prices.size();i++)
+        {
+            if(prices[i]-buy<0)
+                buy=prices[i];
+            res=max(res, prices[i]-buy);
+        }
+        return res;
+    }
+};
+// @lc code=end
+
